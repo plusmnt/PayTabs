ce35fc690171b14f2a49b423d264c4f08f60f93a
plusmnt <mnt@plusmnt.com>
2015-08-21 18:57:46 +0300

    Remove testing values

--- paytabs_config.php
+++ paytabs_config.php
@@ -1,10 +1,11 @@
 <?php
 
 return array(
-    'merchant_email' => 'muh_san@msn.com',
-	'secret_key' => '38Z0c4JvzWGHe2OdUEXgNXLOpmBHgx0jG0jT43oEZ5ld7ar2OoiBm4qLr1tHzu0fuk8znwdnBzBJJ3Lys49O8AAzC4hbyhr2BzCZ',
-	'site_url' => "http://www.plusmnt.com",
-	'return_url' => "http://www.plusmnt.com",
-	'ip_merchant' => "192.168.2.4",
+    'merchant_email' => 'YOUR PAYTABS EMAIL',
+	'secret_key' => 'YOUR SECRET KEY',
+	'site_url' => "YOUR WEBSITE URL",
+	'return_url' => "COMPLETE PAYMENT URL",
+	'ip_merchant' => "WEBSITE IP ADDRESS",
 	'cms_with_version' => "VT_PayTabs 0.1.0"
 );
+?>
